ff851566cbcc2e32fe57978aa920edde5cf814c2
Peter-Brian <studygroup226@gmail.com>
2022-07-01 10:00:04 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int numLeft;
+int numLast;
+for (numLeft = 0; numLeft < 100; numLeft++)
+{
+for (numLast = 0; numLast < 100; numLast++)
+{
+if (numLeft < numLast)
+{
+putchar(numLeft / 10 + 48);
+putchar(numLeft % 10 + 48);
+putchar(32);
+putchar(numLast / 10 + 48);
+putchar(numLast % 10 + 48);
+if (numLeft < 98)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
