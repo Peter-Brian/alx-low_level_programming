3776e5a9e4600195d0a8ba82ab8aa01745e4546e
Peter-Brian <studygroup226@gmail.com>
2022-07-05 02:52:38 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+*/
+int main(void)
+{
+putchar('a');
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This a readme file for nested loops
