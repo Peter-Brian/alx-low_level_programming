e2ddc02c9cb67e8e31c716ddab068c8b09380cc8
PeterBrian <106863315+Peter-Brian@users.noreply.github.com>
2022-07-27 12:04:54 +0300

    Add files via upload

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+
+	dest[a] = '\0';
+
+	return (dest);
+}
